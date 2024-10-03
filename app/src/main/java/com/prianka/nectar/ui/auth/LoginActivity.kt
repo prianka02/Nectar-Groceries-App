2a66a8e7dc2fb384a97bd1842870fc2ae96e58ba
prianka02 <priankaakter115@gmail.com>
2024-10-03 22:42:01 +0600

    Login and SignUp View added

--- app/src/main/java/com/prianka/nectar/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/prianka/nectar/ui/auth/LoginActivity.kt
@@ -1,17 +1,32 @@
 package com.prianka.nectar.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 import com.prianka.nectar.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginBtn: MaterialButton
+    private lateinit var signUpTraverse: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        loginBtn = findViewById(R.id.login_btn)
+        signUpTraverse = findViewById(R.id.sign_up_traverse)
+
+        signUpTraverse.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/prianka/nectar/ui/auth/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.prianka.nectar.ui.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.prianka.nectar.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+
+    }
+}
